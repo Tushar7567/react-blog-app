4c35ce439b36d681725185cca54031ab566ca7ad
Tushar7567 <swain.tushar00@gmail.com>
2022-10-13 19:45:14 +0530

    Create FirstComp.jsx

---
+++ src/componnts/FirstComp.jsx
@@ -0,0 +1,21 @@
+
+const FirstComp = () => {
+    return (
+      <section className="home-grid" style={{marginTop:'40px'}}>
+        <div className="pic-1">
+          <span className="span-1">Lake Bacalar, Mexico</span>{" "}
+          <p className="p">Travel / August 28 2017</p>{" "}
+        </div>
+        <div className="pic-2">
+          <span className="span-2">Lake Lucerne, Switzerland</span>
+          <p className="p">Travel / August 28 2017</p>
+        </div>
+        <div className="pic-3">
+          <span className="span-2">Indian Ocean, India</span>
+          <p className="p">Travel / August 28 2017</p>
+        </div>
+      </section>
+    );
+}
+
+export default FirstComp
