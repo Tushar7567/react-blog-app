e444fbdf4c63382553158e0deed264d995fc8424
Tushar7567 <swain.tushar00@gmail.com>
2022-10-13 19:45:25 +0530

    Create ForthComp.jsx

---
+++ src/componnts/ForthComp.jsx
@@ -0,0 +1,58 @@
+
+const ForthComp = () => {
+    return (
+      <section className="container-1">
+        <span className="text-latest"> The Latest</span>
+        <div className="cont-grid-last">
+          <article className="card-1">
+            <img
+              src="./Images/pexels-tobias-bjørkli-1887624.jpg"
+              height={"230px"}
+              width={"360px"}
+              alt=""
+            />
+            <div>
+              <h3>Somthing is good than nothing</h3>
+              <span>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </span>
+              <p className="p">Travel / August 28 2017</p>
+            </div>
+          </article>
+
+          <article className="card-1">
+            <img
+              src="./Images/pexels-tobias-bjørkli-1887624.jpg"
+              height={"230px"}
+              width={"360px"}
+              alt=""
+            />
+            <div>
+              <h3>Somthing is good than nothing</h3>
+              <span>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </span>
+              <p className="p">Travel / August 28 2017</p>
+            </div>
+          </article>
+          <article className="card-1">
+            <img
+              src="./Images/pexels-tobias-bjørkli-1887624.jpg"
+              height={"230px"}
+              width={"360px"}
+              alt=""
+            />
+            <div>
+              <h3>Somthing is good than nothing</h3>
+              <span>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </span>
+              <p className="p">Travel / August 28 2017</p>
+            </div>
+          </article>
+        </div>
+      </section>
+    );
+}
+
+export default ForthComp
