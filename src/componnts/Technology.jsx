8c3e430c01a3c789b9321473cae0dfffd876df76
Tushar7567 <swain.tushar00@gmail.com>
2022-10-15 17:37:44 +0530

    Update Technology.jsx

--- src/componnts/Technology.jsx
+++ src/componnts/Technology.jsx
@@ -1,22 +1,15 @@
 import React from 'react'
 import ThirdComp from './Third.Comp'
-import { useState,useEffect,} from "react";
+// import { useState,useEffect,} from "react";
 // import axios from 'axios';
 
 
- const Technology = () => {
-  const [obj1, setObj1] = useState([])
+ const Technology = ({obj1, num}) => {
 
-  let passed = require("../mock_data.json")
-  useEffect(()=>{
-    
-    setObj1(passed.technology);
-      
-  },[passed])
   return (
     <div>
       {/* <h1>technology Page</h1> */}
-      <ThirdComp obj1={obj1} />
+      <ThirdComp obj1={obj1} num={num} />
 
     </div>
   )
