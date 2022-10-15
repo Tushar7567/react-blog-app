3cd5d163b0666b6ac50874c39b4715d237b1cc6e
Tushar7567 <swain.tushar00@gmail.com>
2022-10-15 17:37:12 +0530

    Update FirstComp.jsx

--- src/componnts/FirstComp.jsx
+++ src/componnts/FirstComp.jsx
@@ -1,21 +1,31 @@
+import {Link} from 'react-router-dom'
 
 const FirstComp = () => {
     return (
       <section className="home-grid" style={{marginTop:'40px'}}>
-        <div className="pic-1">
-          <span className="span-1">Lake Bacalar, Mexico</span>{" "}
-          <p className="p">Travel / August 28 2017</p>{" "}
-        </div>
-        <div className="pic-2">
+        <Link className='link-text pic-1' to='/article' >
+        {/* <div className="pic-1"> */}
+          <span className="span-1">Lake Bacalar, Mexico</span>
+          <p className="p">Travel / August 28 2017</p>
+        {/* </div> */}
+        </Link>
+
+        <Link className='link-text pic-2' to='/article' >
+        {/* <div className="pic-2"> */}
           <span className="span-2">Lake Lucerne, Switzerland</span>
           <p className="p">Travel / August 28 2017</p>
-        </div>
-        <div className="pic-3">
+        {/* </div> */}
+        </Link>
+
+        <Link className='link-text pic-3' to='/article'  >
+        {/* <div className="pic-3"> */}
           <span className="span-2">Indian Ocean, India</span>
           <p className="p">Travel / August 28 2017</p>
-        </div>
+        {/* </div> */}
+        </Link>
       </section>
     );
 }
 
+// onClick={()=>{articleNav()}}
 export default FirstComp
