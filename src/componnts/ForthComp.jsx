b9ad83059befc903a74ebb0b3737f2a1b09eee59
Tushar7567 <swain.tushar00@gmail.com>
2022-10-15 17:37:21 +0530

    Update ForthComp.jsx

--- src/componnts/ForthComp.jsx
+++ src/componnts/ForthComp.jsx
@@ -1,58 +1,27 @@
+import { Link } from "react-router-dom";
 
-const ForthComp = () => {
-    return (
-      <section className="container-1">
-        <span className="text-latest"> The Latest</span>
-        <div className="cont-grid-last">
-          <article className="card-1">
-            <img
-              src="./Images/pexels-tobias-bjørkli-1887624.jpg"
-              height={"230px"}
-              width={"360px"}
-              alt=""
-            />
-            <div>
-              <h3>Somthing is good than nothing</h3>
-              <span>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              </span>
-              <p className="p">Travel / August 28 2017</p>
-            </div>
-          </article>
+const ForthComp = ({obj1, num}) => {
+  return (
+    <section className="container-1">
+      <span className="text-latest"> The Latest</span>
+      <div className="  carrousel-2">
+        {obj1.slice(num, num+3).map((item, index) => {
+          return (
+            <Link className='link-text' key={index} to='/article' >
+              <article className="card-1" >
+              <img src={item.img_url} className='card-1-img'  alt="" />
+              <div className='card-1-div-padding'>
+                <h3>{item.h3}</h3>
+                <span>{item.span}</span>
+                <p className="p">{item.p}</p>
+              </div>
+            </article>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
 
-          <article className="card-1">
-            <img
-              src="./Images/pexels-tobias-bjørkli-1887624.jpg"
-              height={"230px"}
-              width={"360px"}
-              alt=""
-            />
-            <div>
-              <h3>Somthing is good than nothing</h3>
-              <span>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              </span>
-              <p className="p">Travel / August 28 2017</p>
-            </div>
-          </article>
-          <article className="card-1">
-            <img
-              src="./Images/pexels-tobias-bjørkli-1887624.jpg"
-              height={"230px"}
-              width={"360px"}
-              alt=""
-            />
-            <div>
-              <h3>Somthing is good than nothing</h3>
-              <span>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              </span>
-              <p className="p">Travel / August 28 2017</p>
-            </div>
-          </article>
-        </div>
-      </section>
-    );
-}
-
-export default ForthComp
+export default ForthComp;
