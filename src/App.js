38d2ff1a2e96e5d0260e76696d0b68a33d00ba1a
Tushar7567 <swain.tushar00@gmail.com>
2022-10-15 17:36:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,72 +1,73 @@
 // import logo from './logo.svg';
 import "./App.css";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./componnts/Navbar";
 import Home from "./componnts/Home";
 import Bollywood from "./componnts/Bollywood";
 import Technology from "./componnts/Technology";
 import Hollywood from "./componnts/Hollywood";
 import Fitness from "./componnts/Fitness";
 import Food from "./componnts/Food";
-// import { useState, useEffect } from "react";
-// import axios from "axios";
-// import FirstComp from './componnts/FirstComp'
-// import SecondComp from './componnts/SecondComp'
-// import ThirdComp from './componnts/Third.Comp'
-// import ForthComp from './componnts/ForthComp'
-// import MyContext from './Context';
+import Article from "./componnts/Article";
+import React, { useState, useEffect } from "react";
 
 
 function App() {
-  // const [users, setUsers] = useState([]);
+  const [obj1, setObj1] = useState([]);
 
-  // useEffect(()=>{
-  //   axios.get('/mock_data.json')
-  //     .then((res)=>{
-  //       setUsers(res.data);
-  //     })
-  //     .catch((err)=>{
-  //       console.log('Error');
-  //     })
-  // },[])
-  // console.log(users);
+  // let passed = require("./mock_data.json")
+  useEffect(() => {
+    const fetchdata = async () => {
+      fetch("/onlydata.json")
+        .then((response) => {
+          return response.json();
+        })
+        .then((myjson) => {
+          setObj1(myjson);
+        });
+    };
+    fetchdata();
+    // axios('./mock_data.json')
+    //   .then((myjson) =>{setObj1(myjson.data)});
+  }, []);
+  console.log(obj1);
+
+  let bollywood = 0;
+  let technology = 13;
+  let hollywood = 26;
+  let fitness = 39;
+  let food = 52;
+  let home = 65;
 
 
   return (
     <>
-      <div className="heading">
-        <span className="heading-the">The</span>
-        <span className="heading-siren">Siren</span>
-      </div>
+      
       <BrowserRouter>
-      <div className='navbar'>
-                <ul>
-                    <li>
-                        <Link  to='/' className='link-text'>Home</Link>
-                    </li>
-                    <li>
-                        <Link  to='/bollywood' className='link-text'>Bollywood</Link>
-                    </li>
-                    <li>
-                        <Link to='/technology' className='link-text'>Technology</Link>
-                    </li>
-                    <li>
-                        <Link  to='/hollywood' className='link-text'>Hollywood</Link>
-                    </li>
-                    <li>
-                        <Link to='/fitness' className='link-text'>Fitness</Link>
-                    </li>
-                    <li>
-                        <Link to='/food' className='link-text'>Food</Link>
-                    </li>
-                </ul>
-            </div>
         <Routes>
-            <Route path="/" element={<Home  />} />
-            <Route path="bollywood" element={<Bollywood />} />
-            <Route path="technology" element={<Technology  />} />
-            <Route path="hollywood" element={<Hollywood  />} />
-            <Route path="fitness" element={<Fitness  />} />
-            <Route path="food" element={<Food  />} />
+          <Route path="/" element={<Navbar />}>
+            <Route 
+            path="/" 
+            element={<Home obj1={obj1} num={home} />} />
+            <Route
+              path="/bollywood"
+              element={<Bollywood obj1={obj1} num={bollywood} />}
+            />
+            <Route
+              path="/technology"
+              element={<Technology obj1={obj1} num={technology} />}
+            />
+            <Route
+              path="/hollywood"
+              element={<Hollywood obj1={obj1} num={hollywood} />}
+            />
+            <Route
+              path="/fitness"
+              element={<Fitness obj1={obj1} num={fitness} />}
+            />
+            <Route path="/food" element={<Food obj1={obj1} num={food} />} />
+          </Route>
+          <Route path="/article" element={<Article />} />
         </Routes>
       </BrowserRouter>
     </>
