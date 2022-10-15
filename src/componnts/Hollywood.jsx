76d4af5200ed18a117c1f575adc674c9084a3b37
Tushar7567 <swain.tushar00@gmail.com>
2022-10-15 17:37:24 +0530

    Update Hollywood.jsx

--- src/componnts/Hollywood.jsx
+++ src/componnts/Hollywood.jsx
@@ -1,22 +1,15 @@
 import React from 'react'
 import ThirdComp from './Third.Comp'
-import { useState,useEffect,} from "react";
+// import { useState,useEffect,} from "react";
 
 
 
- const Hollywood = () => {
-  const [obj1, setObj1] = useState([])
+ const Hollywood = ({obj1, num}) => {
 
-  let passed = require("../mock_data.json")
-  useEffect(()=>{
-    
-    setObj1(passed.Hollywood);
-      
-  },[passed])
   return (
     <div>
       {/* <h1>hollywood Page</h1> */}
-      <ThirdComp obj1={obj1} />
+      <ThirdComp obj1={obj1} num={num} />
 
       
     </div>
