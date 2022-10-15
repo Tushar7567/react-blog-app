1892fcbedbe88d92515e68e6d8ee54b15e9a56ec
Tushar7567 <swain.tushar00@gmail.com>
2022-10-15 17:37:41 +0530

    Update SecondComp.jsx

--- src/componnts/SecondComp.jsx
+++ src/componnts/SecondComp.jsx
@@ -1,89 +1,27 @@
+import {Link} from 'react-router-dom'
 
- const SecondComp = () => {
+ const SecondComp = ({obj1, num}) => {
+  console.log(obj1);
     return (
       <section className="container-1">
         <span className="text-latest"> The Latest</span>
         <div className="carrousel">
-          <article className="card-1">
-            <img
-              src="./Images/pexels-tobias-bjørkli-1887624.jpg"
-              height={"230px"}
-              width={"360px"}
-              alt=""
-            />
-            <div>
-              <h3>Somthing is good than nothing</h3>
-              <span>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              </span>
-              <p className="p">Travel / August 28 2017</p>
-            </div>
-          </article>
+          {obj1.slice(num,num+8).map((item, index) => {
+                return (
+                  <Link className='link-text' key={index} to='/article' >
+                  <article className="card-1" >
+                    <img src={item.img_url} className='card-1-img' alt="" />
+                    <div className='card-1-div-padding' >
+                      <h3>{item.h3}</h3>
+                      <span>{item.span}</span>
+                      <p className="p">{item.p}</p>
+                    </div>
+                  </article>
+                  </Link>
+                );
+              })}
 
-          <article className="card-1">
-            <img
-              src="./Images/pexels-tobias-bjørkli-1887624.jpg"
-              height={"230px"}
-              width={"360px"}
-              alt=""
-            />
-            <div>
-              <h3>Somthing is good than nothing</h3>
-              <span>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              </span>
-              <p className="p">Travel / August 28 2017</p>
-            </div>
-          </article>
-
-          <article className="card-1">
-            <img
-              src="./Images/pexels-tobias-bjørkli-1887624.jpg"
-              height={"230px"}
-              width={"360px"}
-              alt=""
-            />
-            <div>
-              <h3>Somthing is good than nothing</h3>
-              <span>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              </span>
-              <p className="p">Travel / August 28 2017</p>
-            </div>
-          </article>
-
-          <article className="card-1">
-            <img
-              src="./Images/pexels-tobias-bjørkli-1887624.jpg"
-              height={"230px"}
-              width={"360px"}
-              alt=""
-            />
-            <div>
-              <h3>Somthing is good than nothing</h3>
-              <span>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              </span>
-              <p className="p">Travel / August 28 2017</p>
-            </div>
-          </article>
-
-          <article className="card-1">
-            <img
-              src="./Images/pexels-tobias-bjørkli-1887624.jpg"
-              height={"230px"}
-              width={"360px"}
-              alt=""
-            />
-            <div>
-              <h3>Somthing is good than nothing</h3>
-              <span>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              </span>
-              <p className="p">Travel / August 28 2017</p>
-            </div>
-          </article>
-        </div>
+        </div> 
       </section>
     );
 }
