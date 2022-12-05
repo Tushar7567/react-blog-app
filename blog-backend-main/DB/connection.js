c7ee494ad7d866a5a19cea4d66a22f3710876166
Tushar7567 <swain.tushar00@gmail.com>
2022-12-05 20:21:39 +0530

    ok

---
+++ blog-backend-main/DB/connection.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+require('dotenv').config({path:"./config.env"})
+
+const Db = process.env.URI
+
+
+mongoose.connect(Db).then(() => {
+    console.log('Database connection successful');
+}).catch((err) => {
+    console.log('Database failed to connect');
+})
